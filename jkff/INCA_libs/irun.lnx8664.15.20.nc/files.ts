345fc4ed8f6e0f7346fb899363f607a5e02fe322
01fe19bec096 <59965108+01fe19bec096@users.noreply.github.com>
2022-04-04 20:45:41 +0530

    Add files via upload

---
+++ jkff/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,5 @@
+1638530748 /home/adld25/jkff/cds.lib
+1638530758 /home/adld25/jkff/hdl.var
+1638271895 /home/adld25/cds.lib
+1638271907 /home/adld25/hdl.var
+1638595742 /home/adld25/jkff/jkff_tb.v
---
+++ jkff/genus.cmd
@@ -0,0 +1,6 @@
+# Cadence Genus(TM) Synthesis Solution, Version 17.22-s017_1, built Apr  1 2018
+
+# Date: Fri Dec 03 16:24:57 2021
+# Host: VLSI12 (x86_64 w/Linux 2.6.32-431.el6.x86_64) (4cores*8cpus*1physical cpu*Intel(R) Core(TM) i7-4770 CPU @ 3.40GHz 8192KB)
+# OS:   Red Hat Enterprise Linux Client release 6.5 (Santiago)
+
